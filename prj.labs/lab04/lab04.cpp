62acfa2e0587a4fea779a8e98a1b13621822e8a5
ngumbk <chulkov.nikita01@gmail.com>
2022-03-22 21:30:14 +0300

    Lab04 fixed + sources remade for vs2022

--- prj.labs/lab04/lab04.cpp
+++ prj.labs/lab04/lab04.cpp
@@ -31,7 +31,7 @@ int process_video(std::string video_name) {
 	int nFrames = video.get(cv::CAP_PROP_FRAME_COUNT);
 
 	//arrays for each stage of frame processing
-	cv::Mat frames[3], framesGS[3], framesBin[3], framesMorph[3], framesCC[3];
+	cv::Mat frame, frameGS, frameBin, frameMorph, frameCC;
 
 	for (int i = 0; i < 3; i++) {
 		//counting current frame
@@ -39,50 +39,50 @@ int process_video(std::string video_name) {
 
 		//getting the current frame
 		video.set(cv::CAP_PROP_POS_FRAMES, frame_number);
-		video >> frames[i];
+		video >> frame;
 
 		//saving orig img
-		cv::imwrite("frames/" + video_name + "_ORIG_" + std::to_string(i + 1) + ".png", frames[i]);
+		cv::imwrite("frames/" + video_name + "_ORIG_" + std::to_string(i + 1) + ".png", frame);
 
 		//color reduction & binarization
-		cv::cvtColor(frames[i], framesGS[i], cv::COLOR_BGR2GRAY); //color reduction
-		cv::imwrite("frames/" + video_name + "_GS_" + std::to_string(i + 1) + ".png", framesGS[i]); //saving gs img
+		cv::cvtColor(frame, frameGS, cv::COLOR_BGR2GRAY); //color reduction
+		cv::imwrite("frames/" + video_name + "_GS_" + std::to_string(i + 1) + ".png", frameGS); //saving gs img
 
-		cv::threshold(framesGS[i], framesBin[i], 188, 255, cv::THRESH_BINARY); //binarization
+		cv::threshold(frameGS, frameBin, 188, 255, cv::THRESH_BINARY); //binarization
 
 			//inverting a binarization in case of high percentage of white pixels
 		double white_pixels_percentage = 0;
-		for (int j = 0; j < framesBin[i].rows; j++) {
-			for (int k = 0; k < framesBin[i].cols; k++) {
-				if (framesBin[i].at<uint8_t>(j, k) == 255) {
+		for (int j = 0; j < frameBin.rows; j++) {
+			for (int k = 0; k < frameBin.cols; k++) {
+				if (frameBin.at<uint8_t>(j, k) == 255) {
 					white_pixels_percentage++;
 				}
 			}
 		}
-		white_pixels_percentage = white_pixels_percentage / (framesBin[i].rows * framesBin[i].cols) * 100;
+		white_pixels_percentage = white_pixels_percentage / (frameBin.rows * frameBin.cols) * 100;
 		if (white_pixels_percentage > 50) {
-			cv::bitwise_not(framesBin[i], framesBin[i]);
+			cv::bitwise_not(frameBin, frameBin);
 		}
 
-		cv::imwrite("frames/" + video_name + "_BIN_" + std::to_string(i + 1) + ".png", framesBin[i]); //saving bin img
+		cv::imwrite("frames/" + video_name + "_BIN_" + std::to_string(i + 1) + ".png", frameBin); //saving bin img
 
 		//morphology
 
 		cv::Mat structuring_element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(6, 12));
 
-		cv::morphologyEx(framesBin[i], framesMorph[i], cv::MORPH_CLOSE, structuring_element);
-		cv::morphologyEx(framesMorph[i], framesMorph[i], cv::MORPH_OPEN, structuring_element);
+		cv::morphologyEx(frameBin, frameMorph, cv::MORPH_CLOSE, structuring_element);
+		cv::morphologyEx(frameMorph, frameMorph, cv::MORPH_OPEN, structuring_element);
 
-		structuring_element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(50, 50));
-		cv::morphologyEx(framesMorph[i], framesMorph[i], cv::MORPH_DILATE, structuring_element);
+		structuring_element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(100, 50));
+		cv::morphologyEx(frameMorph, frameMorph, cv::MORPH_CLOSE, structuring_element);
 
-		cv::imwrite("frames/" + video_name + "_MORPH_" + std::to_string(i + 1) + ".png", framesMorph[i]); //saving bin img
+		cv::imwrite("frames/" + video_name + "_MORPH_" + std::to_string(i + 1) + ".png", frameMorph); //saving bin img
 
 		//finding the main connected component
 			//arrays for connecteedComponentsWithStats output
-		cv::Mat labeledImage(framesMorph[i].size(), CV_32S), stats, centroids;
+		cv::Mat labeledImage(frameMorph.size(), CV_32S), stats, centroids;
 			//labeling connected components and gathering stats
-		int nLabels = cv::connectedComponentsWithStats(framesMorph[i], labeledImage, stats, centroids, 8, CV_32S);
+		int nLabels = cv::connectedComponentsWithStats(frameMorph, labeledImage, stats, centroids, 8, CV_32S);
 
 			//finding main connected components by area, providing it's the biggest except background
 		int max_area = 0, max_label = 1;
@@ -95,28 +95,54 @@ int process_video(std::string video_name) {
 		}
 
 			//creating vector of colors
-		std::vector<cv::Vec3b> colors(nLabels);
+		std::vector<uint8_t> colors(nLabels);
 		for (int j = 0; j < nLabels; j++) {
-			colors[j] = cv::Vec3b(0, 0, 0);
+			colors[j] = 0;
 		}
-		colors[max_label] = cv::Vec3b(255, 255, 255);
+		colors[max_label] = 255;
 		
-		framesCC[i] = frames[i].clone();
-		framesCC[i] = 0;
+		frameCC = frame.clone();
+		cv::cvtColor(frameCC, frameCC, cv::COLOR_BGR2GRAY);
+		frameCC = 0;
 
 			//leaving the main component only visible
 		for (int j = 0; j < labeledImage.rows; j++) {
 			for (int k = 0; k < labeledImage.cols; k++) {
 				int label = labeledImage.at<int>(j, k);
-				cv::Vec3b& pixel = framesCC[i].at<cv::Vec3b>(j, k);
+				uint8_t& pixel = frameCC.at<uint8_t>(j, k);
 				pixel = colors[label];
 			}
 		}
-		cv::imwrite("frames/" + video_name + "_CC_" + std::to_string(i + 1) + ".png", framesCC[i]); //saving img with CCs
+		cv::imwrite("frames/" + video_name + "_CC_" + std::to_string(i + 1) + ".png", frameCC); //saving img with CCs
 
 		//estimating the quality of the mask, we've got
-		cv::Mat standard_mask = cv::imread("../../../data/masks_lab04/" + video_name + "_MASK_" + std::to_string(i + 1) + ".png");
-		std::cout << "Quality of " + video_name + " " << i + 1 << ": " << estimate_quality(framesCC[i], standard_mask) << std::endl;
+		cv::Mat standard_mask = cv::imread("../../../data/masks_lab04/" + video_name + "_MASK_" + std::to_string(i + 1) + ".png").clone();
+		cv::cvtColor(standard_mask, standard_mask, cv::COLOR_BGR2GRAY);
+		std::cout << "Quality of " + video_name + " " << i + 1 << ": " << estimate_quality(frameCC, standard_mask) << std::endl;
+
+		//creating an image with mask overlaying original
+		cv::Mat mask_over_original(frameCC.size(), CV_8UC3);
+		mask_over_original = 0;
+			//4 cases - 4 colors
+		for (int j = 0; j < frame.rows; j++) {
+			for (int k = 0; k < frame.cols; k++) {
+				if ((standard_mask.at<uint8_t>(j, k) == 0) && (frameCC.at<uint8_t>(j, k) == 0)) {
+					mask_over_original.at<cv::Vec3b>(j, k) = cv::Vec3b(0, 0, 0);
+				}
+				else if ((standard_mask.at<uint8_t>(j, k) == 0) && (frameCC.at<uint8_t>(j, k) == 255)) {
+					mask_over_original.at<cv::Vec3b>(j, k) = cv::Vec3b(0, 0, 255);
+				}	
+				else if ((standard_mask.at<uint8_t>(j, k) == 255) && (frameCC.at<uint8_t>(j, k) == 0)) {
+					mask_over_original.at<cv::Vec3b>(j, k) = cv::Vec3b(255, 0, 0);
+				}
+				else if ((standard_mask.at<uint8_t>(j, k) == 255) && (frameCC.at<uint8_t>(j, k) == 255)) {
+					mask_over_original.at<cv::Vec3b>(j, k) = cv::Vec3b(255, 255, 255);
+				}
+			}
+		}
+			//combining with the original
+		cv::addWeighted(frame, 0.5, mask_over_original, 0.5, 0.0, mask_over_original);
+		cv::imwrite("frames/" + video_name + "_final_" + std::to_string(i + 1) + ".png", mask_over_original); //saving the final img
 	}
 	std::cout << std::endl;
 
